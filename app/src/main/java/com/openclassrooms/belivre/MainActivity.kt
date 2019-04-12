c8d7a469d2469c49a63e7d7ec8ec7f7e80a5583c
troturier <thibaultroturier@gmail.com>
2019-04-12 11:59:12 +0200

    Ajout du support pour Crashlytics

--- app/src/main/java/com/openclassrooms/belivre/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/belivre/MainActivity.kt
@@ -2,11 +2,14 @@ package com.openclassrooms.belivre
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
 }
