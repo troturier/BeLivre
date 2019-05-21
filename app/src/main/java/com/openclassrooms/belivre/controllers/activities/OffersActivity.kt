53a17d56434eaa2e1ff5c77e48edccfc6f799508
troturier <thibaultroturier@gmail.com>
2019-05-21 22:21:01 +0200

    Ajout de l'interface de demande d'emprunt lors de la sélection d'un élément dans l'activité OffersActivity

--- app/src/main/java/com/openclassrooms/belivre/controllers/activities/OffersActivity.kt
+++ app/src/main/java/com/openclassrooms/belivre/controllers/activities/OffersActivity.kt
@@ -1,8 +1,10 @@
 package com.openclassrooms.belivre.controllers.activities
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
@@ -15,10 +17,11 @@ import com.openclassrooms.belivre.adapters.OffersRecyclerViewAdapter
 import com.openclassrooms.belivre.models.Book
 import com.openclassrooms.belivre.models.User
 import com.openclassrooms.belivre.models.UserBook
-import com.openclassrooms.belivre.utils.toast
+import com.openclassrooms.belivre.utils.loadProfilePictureIntoImageView
 import com.openclassrooms.belivre.viewmodels.BaseViewModelFactory
 import com.openclassrooms.belivre.viewmodels.UserBookViewModel
 import kotlinx.android.synthetic.main.activity_offers.*
+import kotlinx.android.synthetic.main.offers_dialog.view.*
 
 class OffersActivity : AppCompatActivity(), LifecycleOwner {
 
@@ -64,11 +67,55 @@ class OffersActivity : AppCompatActivity(), LifecycleOwner {
                 }
             }
             adapter = OffersRecyclerViewAdapter(sortedList!!){item: UserBook, _: Int ->
-                toast(item.userDisplayName.toString())
+                showOffersDialog(item)
             }
             offersRV.adapter = adapter
             adapter.notifyDataSetChanged()
         }
+    }
+
+    @SuppressLint("InflateParams")
+    private fun showOffersDialog(userBook:UserBook){
+        val layoutInflater = this.layoutInflater
+        val dialogView = layoutInflater.inflate(R.layout.offers_dialog, null)
+
+        dialogView.offersDialogTV.text = getString(R.string.borrowed_dialog_dn_borrowed_tab, userBook.userDisplayName)
+
+        dialogView.textView7.text = userBook.title
+
+        loadProfilePictureIntoImageView(dialogView.offersDialogIV, this, userBook.userPicUrl, userBook.userId.toString())
+
+        val alertDialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .setNeutralButton(getString(R.string.cancel)) { _, _ -> }
+
+        val dialog = alertDialog.create()
+
+        dialogView.requestButtonOffersDialog.setOnClickListener {
+            val alertDialogC = AlertDialog.Builder(this)
+                .setTitle(getString(R.string.send_a_borrowing_request))
+                .setMessage(getString(R.string.are_you_sure_request_borrow))
+                .setPositiveButton(getString(R.string.yes)) { dialogCS, _ ->
+                    userBook.requestSenderDisplayName = getString(R.string.profile_display_name, user.firstname, user.lastname?.substring(0,1))
+                    userBook.requestSenderId = user.id
+                    userBook.requestSenderPicUrl = user.profilePicURL
+
+                    userBook.status = 2
+
+                    userBookVM.updateUserBook(userBook)
+
+                    dialogCS.dismiss()
+                    dialog.dismiss()
+                    finish()
+                }
+                .setNegativeButton(getString(R.string.no)) { dialogCNS, _ ->
+                    dialogCNS.dismiss()
+                }
+            val dialogC = alertDialogC.create()
+            dialogC.show()
+        }
+
+        dialog.show()
     }
 
     override fun onSupportNavigateUp(): Boolean {
