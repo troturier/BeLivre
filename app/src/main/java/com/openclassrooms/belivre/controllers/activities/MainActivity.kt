d98a9e20004eb922cdf38b973027271fd68ef669
troturier <thibaultroturier@gmail.com>
2019-05-22 17:37:11 +0200

    Ajout d'une BottomNavigationBar + Fragments associés

--- app/src/main/java/com/openclassrooms/belivre/controllers/activities/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/belivre/controllers/activities/MainActivity.kt
@@ -3,23 +3,30 @@ package com.openclassrooms.belivre.controllers.activities
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
 import com.crashlytics.android.Crashlytics
 import com.firebase.ui.auth.AuthUI
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.openclassrooms.belivre.R
+import com.openclassrooms.belivre.controllers.fragments.ChatFragment
+import com.openclassrooms.belivre.controllers.fragments.ListFragment
+import com.openclassrooms.belivre.controllers.fragments.MapFragment
 import com.openclassrooms.belivre.models.User
 import com.openclassrooms.belivre.models.UserBook
 import com.openclassrooms.belivre.utils.displayNotificationOnDrawer
@@ -30,12 +37,12 @@ import com.openclassrooms.belivre.viewmodels.BaseViewModelFactory
 import com.openclassrooms.belivre.viewmodels.UserBookViewModel
 import com.openclassrooms.belivre.viewmodels.UserViewModel
 import io.fabric.sdk.android.Fabric
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.nav_header.*
 import com.openclassrooms.belivre.controllers.activities.ProfileActivity as ProfileActivity1
 
 
-class MainActivity : AppCompatActivity(), LifecycleOwner {
-
+class MainActivity : AppCompatActivity(), LifecycleOwner, AHBottomNavigation.OnTabSelectedListener {
 
     //FOR DATA
 
@@ -116,6 +123,28 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
 
         if(currentUser == null) startSignInActivity(this)
         else { userVM.getUser(currentUser!!.uid).observe(this, Observer { user:User? -> checkUserProfileComplete(user)}) }
+
+        configureBottomNav()
+    }
+
+    private fun configureBottomNav(){
+        val mapItem = AHBottomNavigationItem(getString(R.string.map), ContextCompat.getDrawable(this, R.drawable.ic_map), ContextCompat.getColor(this, R.color.colorPrimaryDark))
+        val listItem = AHBottomNavigationItem(getString(R.string.list), ContextCompat.getDrawable(this, R.drawable.ic_list), ContextCompat.getColor(this, R.color.colorPrimary))
+        val chatItem = AHBottomNavigationItem(getString(R.string.chat), ContextCompat.getDrawable(this, R.drawable.ic_chat), ContextCompat.getColor(this, R.color.colorAccent))
+
+        bottom_navigation_main.addItem(mapItem)
+        bottom_navigation_main.addItem(listItem)
+        bottom_navigation_main.addItem(chatItem)
+
+        bottom_navigation_main.defaultBackgroundColor = ContextCompat.getColor(this, R.color.colorPrimaryDark)
+        bottom_navigation_main.accentColor = ContextCompat.getColor(this, R.color.colorAccent)
+        bottom_navigation_main.inactiveColor = Color.WHITE
+
+        bottom_navigation_main.isColored = true
+
+        bottom_navigation_main.setOnTabSelectedListener(this)
+
+        bottom_navigation_main.currentItem = 0
     }
 
     //appbar - toolbar button click
@@ -145,7 +174,6 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
             drawer_username.text = getString(R.string.profile_display_name, user2.firstname, user2.lastname?.substring(0,1))
             drawer_email.text = user2.email
             loadProfilePictureIntoImageView(drawer_imageview_profile, this, user2.profilePicURL, user2.id.toString())
-
 
             userBookVM.getUserBooksByUserId(user.id.toString()).observe(this, Observer { userBooks: List<UserBook>? -> displayNotificationOnDrawer(userBooks, this, this, libraryCount)})
         }
@@ -184,6 +212,24 @@ class MainActivity : AppCompatActivity(), LifecycleOwner {
                 startSignInActivity(this)
             }
         }
+    }
+
+    override fun onTabSelected(position: Int, wasSelected: Boolean): Boolean {
+        when(position){
+            0 -> {
+                val mapFragment = MapFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_main, mapFragment).commit()
+            }
+            1 -> {
+                val listFragment = ListFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_main, listFragment).commit()
+            }
+            else -> {
+                val chatFragment = ChatFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_main, chatFragment).commit()
+            }
+        }
+        return true
     }
 
     companion object {
