fda45f34febe4294145823544ecba9183b28d0cc
troturier <thibaultroturier@gmail.com>
2019-06-06 01:26:13 +0200

    Mise à jour de l'affichage des messages

--- app/src/main/java/com/openclassrooms/belivre/chat/recyclerViewItems/MessageItem.kt
+++ app/src/main/java/com/openclassrooms/belivre/chat/recyclerViewItems/MessageItem.kt
@@ -2,13 +2,13 @@ package com.openclassrooms.belivre.chat.recyclerViewItems
 
 import android.view.Gravity
 import android.widget.FrameLayout
+import androidx.core.content.ContextCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.openclassrooms.belivre.R
 import com.openclassrooms.belivre.chat.Message
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.item_text_message.*
-import org.jetbrains.anko.backgroundResource
 import org.jetbrains.anko.wrapContent
 import java.text.SimpleDateFormat
 import java.util.*
@@ -23,7 +23,7 @@ abstract class MessageItem(private val message: Message)
     }
 
     private fun setTimeText(viewHolder: ViewHolder) {
-        val sdfDate = SimpleDateFormat("dd/M/yyyy", Locale.getDefault())
+        val sdfDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
         val sdfTime = SimpleDateFormat("HH:mm", Locale.getDefault())
         val currentDate = sdfDate.format(Date())
         if(sdfDate.format(message.time) == currentDate){
@@ -37,14 +37,14 @@ abstract class MessageItem(private val message: Message)
     private fun setMessageRootGravity(viewHolder: ViewHolder) {
         if (message.senderId == FirebaseAuth.getInstance().currentUser?.uid) {
             viewHolder.message_root.apply {
-                backgroundResource = R.drawable.rect_round_accent_color
+                viewHolder.textView_message_text.background = ContextCompat.getDrawable(viewHolder.containerView.context, R.drawable.rect_round_accent_color)
                 val lParams = FrameLayout.LayoutParams(wrapContent, wrapContent, Gravity.END)
                 this.layoutParams = lParams
             }
         }
         else {
             viewHolder.message_root.apply {
-                backgroundResource = R.drawable.rect_round_primary_color
+                viewHolder.textView_message_text.background = ContextCompat.getDrawable(viewHolder.containerView.context, R.drawable.rect_round_primary_color)
                 val lParams = FrameLayout.LayoutParams(wrapContent, wrapContent, Gravity.START)
                 this.layoutParams = lParams
             }
